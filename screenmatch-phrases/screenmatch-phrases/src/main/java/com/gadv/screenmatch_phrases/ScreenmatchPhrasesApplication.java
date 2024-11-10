6e94d897757c5c411032be028a0c2d5e36317aeb
Gustavo Davalos <gadavalosv@gmail.com>
2024-11-10 01:25:38 -0600

    Setup Desafio curso Java: creando tu primera API y conectándola al Front End
    Initial Setup desafio curso Java: creando tu primera API y conectándola al Front End

---
+++ screenmatch-phrases/screenmatch-phrases/src/main/java/com/gadv/screenmatch_phrases/ScreenmatchPhrasesApplication.java
@@ -0,0 +1,13 @@
+package com.gadv.screenmatch_phrases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchPhrasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchPhrasesApplication.class, args);
+	}
+
+}
